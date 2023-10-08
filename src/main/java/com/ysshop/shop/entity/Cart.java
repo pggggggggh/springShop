e796abe0eb927b7d8f9789e3367114fbbc0fbd81
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-09 08:38:09 +0900

    Update Cart.java

--- src/main/java/com/ysshop/shop/entity/Cart.java
+++ src/main/java/com/ysshop/shop/entity/Cart.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = "carts")
+@NoArgsConstructor
 public class Cart extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
