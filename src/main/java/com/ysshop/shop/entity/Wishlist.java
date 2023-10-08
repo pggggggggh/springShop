73e27cbc5bf316cabed94f95ced1900de0117146
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-09 08:44:20 +0900

    Update Wishlist.java

--- src/main/java/com/ysshop/shop/entity/Wishlist.java
+++ src/main/java/com/ysshop/shop/entity/Wishlist.java
@@ -4,19 +4,27 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
 @ToString
 @Entity
 @Table(name = "wishlists")
+@NoArgsConstructor
 public class Wishlist extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long wishlistId;
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; // 찜한 사용자 정보
 
     @ManyToOne
     @JoinColumn(name = "product_id")
-    private Product product;
+    private Product product; // 찜한 상품 정보
+    
+    private Date addedDate; // 상품이 찜 목록에 추가된 날짜
 }
 
