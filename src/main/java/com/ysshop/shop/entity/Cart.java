6f93ff737565ce97b5899b4efa3eb82a5169b643
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-11 07:01:38 +0900

    Update Cart.java

--- src/main/java/com/ysshop/shop/entity/Cart.java
+++ src/main/java/com/ysshop/shop/entity/Cart.java
@@ -14,18 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cart extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @ManyToOne
+    @OneToOne
     @JoinColumn(name = "user_id")
     private User user; // 인플루언서 정보
-
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product; // 상품 정보
-
-    private int quantity; // 상품 수량
-
+    
 }
 
