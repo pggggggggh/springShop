06a3df15dde3b04bc2475d3a5e89520051643c3c
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-11 07:31:17 +0900

    Update Order.java

--- src/main/java/com/ysshop/shop/entity/Order.java
+++ src/main/java/com/ysshop/shop/entity/Order.java
@@ -1,9 +1,14 @@
 package com.ysshop.shop.entity;
 
+import com.ysshop.shop.constant.OrderStatus;
+import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
-import javax.persistence.*;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,13 +28,14 @@ public class Order extends BaseEntity {
     private User user; // 주문한 사용자 정보
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<OrderItem> orderItems; // 주문 상품 목록
+    private List<OrderProduct> orderProducts = new ArrayList<>(); // 주문 상품 목록
 
     private String deliveryAddress; // 배송 주소
     private String deliveryMethod; // 배송 방법: 일반택배, 당일특급 등
     private String paymentMethod; // 결제 방법: 토스 페이 등
-    private Date orderDate; // 주문 날짜
-    private String orderStatus; // 주문 상태: 주문 확인, 배송 중, 배송 완료 등
+    private LocalDateTime orderDate; // 주문 날짜
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문 상태: 주문 확인, 배송 중, 배송 완료 등
     private String soldOutProcess; // 품절 상품 처리 방법: 전체 환불, 전체 미송, 상품별 선택 등
     
     // 결제 정보
@@ -41,4 +47,3 @@ public class Order extends BaseEntity {
     // 주문/결제 동의 관련
     private boolean isAgreeWithTerms; // 주문상품 및 결제 대행 이용약관 동의
 }
-
