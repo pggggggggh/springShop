e820d5b34c567bf02b3164cc172753b4c0002205
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-11 06:50:01 +0900

    Update ProductRepository.java

--- src/main/java/com/ysshop/shop/repository/ProductRepository.java
+++ src/main/java/com/ysshop/shop/repository/ProductRepository.java
@@ -4,4 +4,8 @@ import com.ysshop.shop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  List<Product> findByName(String name);
+
+    @Query(value = "select i from Product i where i.additionalDescription like %:additionalDescription% order by i.price desc", nativeQuery = true)
+    List<Product> findByadditionalDescription(@Param("additionalDescription") String additionalDescription);
 }
