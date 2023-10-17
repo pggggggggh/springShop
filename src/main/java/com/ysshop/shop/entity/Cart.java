97c89217c6e57d06765001c0c3c3b194291b2e03
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-17 13:22:18 +0900

    Update Cart.java

--- src/main/java/com/ysshop/shop/entity/Cart.java
+++ src/main/java/com/ysshop/shop/entity/Cart.java
@@ -21,6 +21,12 @@ public class Cart extends BaseEntity {
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user; // 인플루언서 정보
+
+    public static Cart createCart(User user) {
+        Cart cart = new Cart();
+        cart.setUser(user);
+        return cart;
+    }
     
 }
 
