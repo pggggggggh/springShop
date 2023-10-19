eab9021cdb7b850f520a521ea512883391d06776
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-20 06:43:00 +0900

    Create ProductSearchDto.java

---
+++ src/main/java/com/ysshop/shop/dto/ProductSearchDto.java
@@ -0,0 +1,19 @@
+package com.ysshop.shop.dto;
+import com.ysshop.shop.constant.BodyType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ProductSearchDto {
+
+    private String searchDateType;
+
+    private Boolean searchIsSoldOut;        // 상품의 판매상태를 기준으로 상품 데이터 조회
+
+    private BodyType searchBodyType;              // 상품의 카테고리를 기준으로 상품 데이터 조회
+
+    private String searchBy;                        // 상품 조회시 어떤 유형으로 조회할지 선택
+    // itemNm : 상품명, createdBy : 상품 등록자 아이디
+    private String searchQuery = "";                // 조회할 검색어 저장할 변수
+
+}
