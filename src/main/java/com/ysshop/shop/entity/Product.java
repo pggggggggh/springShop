6f2e43c1d86b5a69ec74df85146ebd3dd84b95bb
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-23 05:06:14 +0900

    Update Product.java

--- src/main/java/com/ysshop/shop/entity/Product.java
+++ src/main/java/com/ysshop/shop/entity/Product.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import java.util.List;
 import com.ysshop.shop.exception.OutOfStockException;
 import java.time.LocalDateTime;
+import com.ysshop.shop.dto.ProductFormDto;
 @Getter
 @Setter
 @ToString
