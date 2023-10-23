d91bb90567087dd05fbb631b6e7f874d462c3cee
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-23 12:34:38 +0900

    Update ProductController.java

--- src/main/java/com/ysshop/shop/controller/ProductController.java
+++ src/main/java/com/ysshop/shop/controller/ProductController.java
@@ -1,22 +1,48 @@
 package com.ysshop.shop.controller;
-
+import com.ysshop.shop.service.ProductService;
 import com.ysshop.shop.dto.ProductFormDto;
+import com.ysshop.shop.dto.ProductSearchDto;
+import com.ysshop.shop.entity.Product;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.HashMap;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/seller") // 판매자가 직접 상품을 업로드
 public class ProductController {
-    @PostMapping("/new/products")
+    private final ProductService productService;
+    @PostMapping("/seller/new/products")
     public void upload(@Valid @RequestPart(value="dto") ProductFormDto productFormDto,
                        @NotEmpty @RequestPart(value="repImg") MultipartFile repImg,
                        @RequestPart(value="otherImg", required = false) List<MultipartFile> otherImg) {
         System.out.println(otherImg.size());
+    }
+    //코드 생략
+    //상품관리자 조회
+    @GetMapping("/api/admin/products")
+    public ResponseEntity<Map<String, Object>> getItems(ProductSearchDto productSearchDto,
+                                                        @RequestParam(value = "page", defaultValue = "0") int page) {
+        Pageable pageable = PageRequest.of(page, 3);
+        Page<Product> products = productService.getAdminProductPage(productSearchDto, pageable);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("products", products.getContent());
+        response.put("currentPage", products.getNumber());
+        response.put("totalProducts", products.getTotalElements());
+        response.put("totalPages", products.getTotalPages());
+        response.put("productSearchDto", productSearchDto);
+        response.put("maxPage", 5);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
