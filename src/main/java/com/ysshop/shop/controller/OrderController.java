cd58e4ee919052b0a0e24561037e4bd2dad1dd62
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-23 12:33:42 +0900

    Update OrderController.java

--- src/main/java/com/ysshop/shop/controller/OrderController.java
+++ src/main/java/com/ysshop/shop/controller/OrderController.java
@@ -8,22 +8,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
+import com.ysshop.shop.dto.OrderHistDto;
+import java.util.HashMap;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OrderController {
 
     private final OrderService orderService;
 
     @PostMapping(value = "/order")
-    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto
+    public @ResponseBody ResponseEntity<?> order(@RequestBody @Valid OrderDto orderDto
             , BindingResult bindingResult, Principal principal){
         if(bindingResult.hasErrors()){
             StringBuilder sb = new StringBuilder();
@@ -45,5 +48,17 @@ public class OrderController {
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public ResponseEntity<Map<String, Object>> getOrderHistory(@RequestParam(value = "page", defaultValue = "0") int page, Principal principal) {
+        Pageable pageable = PageRequest.of(page, 4);
+        Page<OrderHistDto> ordersHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("orders", ordersHistDtoList);
+        response.put("page", pageable.getPageNumber());
+        response.put("maxPage", 5);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
