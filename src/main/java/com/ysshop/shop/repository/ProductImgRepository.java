edc12a7ab6b0cb74a7fc8e60e495693b619eea9f
hwangjunyeon0514 <141803633+hwangjunyeon0514@users.noreply.github.com>
2023-10-23 12:47:45 +0900

    Update ProductImgRepository.java

--- src/main/java/com/ysshop/shop/repository/ProductImgRepository.java
+++ src/main/java/com/ysshop/shop/repository/ProductImgRepository.java
@@ -2,6 +2,9 @@ package com.ysshop.shop.repository;
 
 import com.ysshop.shop.entity.ProductImg;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface ProductImgRepository extends JpaRepository<ProductImg, Long> {
+    List<ProductImg> findByProductIdOrderByIdAsc(Long productId);
+
+    ProductImg findByProductIdAndIsRepImg(Long productId, Boolean isRepImg);
 }
